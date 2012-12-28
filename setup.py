a0fc2eb14b62b6b0869229965be1a3e12ba89ff4
Mark McGuire <mark.b.mcg@gmail.com>
2012-12-28 06:53:39 +0000

    set version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'srcdspy',
-    version = '1.0',
+    version = '1.1',
     packages = find_packages(),
     url = 'https://github.com/TronPaul/srcdspy',
     license = 'LGPL',
